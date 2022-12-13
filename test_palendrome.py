995d274bfe9c43816c49ea4c210f5dde3ca63622
Adrian Nettleship <nettlesh@amazon.com>
2022-12-13 21:55:45 +0000

    T test_palendrome_should_return_true_with_two_matching_digits

--- test_palendrome.py
+++ test_palendrome.py
@@ -2,3 +2,6 @@ from palendrome import palendrome
 
 def test_palendrome_should_return_true_with_single_digit_int():
     assert palendrome(1) == True
+
+def test_palendrome_should_return_true_with_two_matching_digits():
+    assert palendrome(11) == True
