d8e5e52e037c375dc45675448e31afd1e3c4a589
Adrian Nettleship <nettlesh@amazon.com>
2022-12-16 11:19:19 +0000

    T test_palendrome_should_return_false_with_two_non_matching_digits

--- palendrome.py
+++ palendrome.py
@@ -1,2 +1,4 @@
 def palendrome(inputstring):
+    if inputstring > 9:
+        return inputstring%11 == 0
     return True
--- test_palendrome.py
+++ test_palendrome.py
@@ -1,4 +1,5 @@
 from palendrome import palendrome
+import pytest
 
 def test_palendrome_should_return_true_with_single_digit_int():
     assert palendrome(1) == True
@@ -6,5 +7,7 @@ def test_palendrome_should_return_true_with_single_digit_int():
 def test_palendrome_should_return_true_with_two_matching_digits():
     assert palendrome(11) == True
 
-def test_palendrome_should_return_false_with_two_non_matching_digits():
-    assert palendrome(12) == False
+input_list = [12, 13, 21, 31, 14]
+@pytest.mark.parametrize("list_element", input_list)
+def test_palendrome_should_return_false_with_two_non_matching_digits(list_element):
+    assert palendrome(list_element) == False
