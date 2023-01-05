26ae3ec03950ebdef23b3df24ff510f8172aba40
Adrian Nettleship <nettlesh@amazon.com>
2023-01-05 22:03:01 +0000

    T test_palendrome_should_return_true_with_four_digits_first_and_last_matching_middle_two_matching

--- palendrome.py
+++ palendrome.py
@@ -1,7 +1,10 @@
 def palendrome(input_int):
     if 10 < input_int < 100:
         return input_int%11 == 0
-    elif 100 <= input_int:
+    elif 100 <= input_int < 1000:
         return input_int // 100 == input_int % 10
+    elif input_int >= 1000:
+        mid = (input_int - (1000 * (input_int // 1000)))//10
+        return (input_int // 1000 == input_int % 10) and palendrome(mid)
     else:
         return True
--- test_palendrome.py
+++ test_palendrome.py
@@ -39,8 +39,13 @@ input_list = [1222, 2333, 4535, 7681, 8293]
 def test_palendroime_should_return_false_with_four_digits_first_and_last_not_matching(list_element):
     assert palendrome(list_element) == False 
 
+
 input_list = [1211, 2313, 4534, 7681, 8293]
 @pytest.mark.parametrize("list_element", input_list)
 def test_palendroime_should_return_false_with_four_digits_middle_two_digits_not_matching(list_element):
     assert palendrome(list_element) == False 
 
+input_list = [1111, 2332, 4554, 8888, 8228]
+@pytest.mark.parametrize("list_element", input_list)
+def test_palendroime_should_return_true_with_four_digits_first_and_last_matching_middle_two_matching(list_element):
+    assert palendrome(list_element) == True 
