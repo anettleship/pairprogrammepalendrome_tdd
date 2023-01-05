0016be6447b6bb1752dc558962edf2d8f237ac42
Adrian Nettleship <nettlesh@amazon.com>
2023-01-05 22:12:46 +0000

    T test_palendrome_should_return_false_with_five_digits_first_and_last_not_matching

--- test_palendrome.py
+++ test_palendrome.py
@@ -30,22 +30,29 @@ def test_palendroime_should_return_false_with_three_digits_first_and_last_not_ma
 
 input_list = [121, 232, 454, 767, 898]
 @pytest.mark.parametrize("list_element", input_list)
-def test_palendroime_should_return_true_with_three_digits_first_and_last_matching(list_element):
+def test_palendrome_should_return_true_with_three_digits_first_and_last_matching(list_element):
     assert palendrome(list_element) == True
 
 
 input_list = [1222, 2333, 4535, 7681, 8293]
 @pytest.mark.parametrize("list_element", input_list)
-def test_palendroime_should_return_false_with_four_digits_first_and_last_not_matching(list_element):
+def test_palendrome_should_return_false_with_four_digits_first_and_last_not_matching(list_element):
     assert palendrome(list_element) == False 
 
 
 input_list = [1211, 2313, 4534, 7681, 8293]
 @pytest.mark.parametrize("list_element", input_list)
-def test_palendroime_should_return_false_with_four_digits_middle_two_digits_not_matching(list_element):
-    assert palendrome(list_element) == False 
+def test_palendrome_should_return_false_with_four_digits_middle_two_digits_not_matching(list_element):
+    assert palendrome(list_element) == False
+
 
 input_list = [1111, 2332, 4554, 8888, 8228]
 @pytest.mark.parametrize("list_element", input_list)
-def test_palendroime_should_return_true_with_four_digits_first_and_last_matching_middle_two_matching(list_element):
+def test_palendrome_should_return_true_with_four_digits_first_and_last_matching_middle_two_matching(list_element):
     assert palendrome(list_element) == True 
+
+
+input_list = [12122, 23233, 45335, 76481, 82593]
+@pytest.mark.parametrize("list_element", input_list)
+def test_palendrome_should_return_false_with_five_digits_first_and_last_not_matching(list_element):
+    assert palendrome(list_element) == False 
