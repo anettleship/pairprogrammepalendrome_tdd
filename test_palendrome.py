b8d6e62c27a9bad70152407cd57cb30b0d20ef7b
Adrian Nettleship <nettlesh@amazon.com>
2023-01-05 20:09:03 +0000

    T test_palendroime_should_return_false_with_four_digits_middle_two_digits_not_matching

--- test_palendrome.py
+++ test_palendrome.py
@@ -39,3 +39,8 @@ input_list = [1222, 2333, 4535, 7681, 8293]
 def test_palendroime_should_return_false_with_four_digits_first_and_last_not_matching(list_element):
     assert palendrome(list_element) == False 
 
+input_list = [1211, 2313, 4534, 7681, 8293]
+@pytest.mark.parametrize("list_element", input_list)
+def test_palendroime_should_return_false_with_four_digits_middle_two_digits_not_matching(list_element):
+    assert palendrome(list_element) == False 
+
